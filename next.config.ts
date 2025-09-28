e4dbb2ba8e7e1d6d9298a758d4ab019fe8e801ce
rymetry <ryoma.saito2@gmail.com>
2025-09-29 04:34:08 +0900

    rymlabx-initial-commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.microcms-assets.io',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.microcms.io',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
